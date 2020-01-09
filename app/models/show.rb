6c4d3d5c2c93766c4a6612e913686af8081ef6a4
Gregory Losco <gtlosco@gmail.com>
2020-01-09 01:39:45 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,5 +11,9 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end
+  
+  def self.most_popular_show
+    self.where("rating=?", self.highest_rating).first
+  end
 
 end
